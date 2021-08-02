3ccc64b1439a89fea916b3e8ddc7d6ab02fe3191
SteveParadox <39016370+SteveParadox@users.noreply.github.com>
2021-08-02 09:53:28 +0100

    Update __init__.py

--- Backend/__init__.py
+++ Backend/__init__.py
@@ -4,7 +4,6 @@ from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from flask_mail import Mail
 from flask_marshmallow import Marshmallow
-from flask_socketio import SocketIO
 from flask_sqlalchemy import SQLAlchemy
 from Backend.config import Config
 from flask_profiler import Profiler
