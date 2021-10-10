75a4305dfe9eab79d4d5185358de96a5a3ca2b83
SteveParadox <39016370+SteveParadox@users.noreply.github.com>
2021-10-10 07:34:37 +0100

    Add files via upload

--- Backend/records/routes.py
+++ Backend/records/routes.py
@@ -19,7 +19,7 @@ def registeredCourses(student_id):
     return render_template('', record=record)
 
 @records.route('/view/result/<string:student_id>', methods=['GET'])
-@login_required
+@login_required 
 @check_confirmed
 def viewResult(student_id):
     detail = Student.query.filter_by(unique_id=unique_id).first()
@@ -31,4 +31,49 @@ def viewResult(student_id):
 def clear():
     #db.drop_all(app=create_app())
     db.create_all(app=create_app())
+    return redirect(url_for('student.home'))
+
+
+@records.route('/faculty/add/science')
+def facultyAddScience():
+    faculty = Faculty()
+    faculty.sciences_id= 1
+    data = {"name":["SSLT","Sciences","Engineering", "Basic Medical Sciences"]}
+    for i in data['name']:
+        faculty.name=i
+        db.session.add(faculty)
+    db.session.commit()
+    return redirect(url_for('student.home'))
+
+@records.route('/faculty/add/socialscience')
+def facultyAddSocialScience():
+    faculty = Faculty()
+    faculty.socialsciences_id= 1
+    data = {"name":["Marketting","Management Sciences","Finance and Banking", "Accounting"]}
+    for i in data['name']:
+        faculty.name=i
+        db.session.add(faculty)
+    db.session.commit()
+    return redirect(url_for('student.home'))
+
+@records.route('/faculty/add/art')
+def facultyAddArt():
+    faculty = Faculty()
+    faculty.art_id= 1
+    data = {"name":["Law","Theatre Art","Linguistics and Communication studies", "Adult and non-formal Education"]}
+    for i in data['name']:
+        faculty.name=i
+        db.session.add(faculty)
+    db.session.commit()
+    return redirect(url_for('student.home'))
+
+@records.route('/dept/add/<string:name>')
+def facultyAddname(name):
+    faculty = Faculty()
+    faculty.art_id= 1
+    data = {"name":["Law","Theatre Art","Linguistics and Communication studies", "Adult and non-formal Education"]}
+    for i in data['name']:
+        faculty.name=i
+        db.session.add(faculty)
+    db.session.commit()
     return redirect(url_for('student.home'))
