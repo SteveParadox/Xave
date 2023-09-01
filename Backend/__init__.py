b68dc15f3b59df72d815976eeabc50014a983a66
SteveParadox <fordstphn@gmail.com>
2023-09-01 03:40:53 +0100

    Push

--- Backend/__init__.py
+++ Backend/__init__.py
@@ -1,7 +1,7 @@
 import datetime
 from flask import *
 from flask_bcrypt import Bcrypt
-from flask_jwt_extended import JWTManager
+#from flask_jwt_extended import JWTManager
 from flask_mail import Mail
 from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
